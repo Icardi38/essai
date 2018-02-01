444d5314212ea19fba375693b057e46c97ea8c19
Icardi38 <vm26190@hotmail.fr>
2018-02-01 14:20:55 +0100

    création de categorie
    créer des categories permettant de ranger les livres dans des categories distinct

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131162328) do
+ActiveRecord::Schema.define(version: 20180201130959) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
 end
