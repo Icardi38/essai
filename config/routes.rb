ce30ce31fd03137061f6956e2f4895eaf92b6b25
Icardi38 <vm26190@hotmail.fr>
2018-02-01 09:39:18 +0100

    Formulaire de modification
    création d'un formulaire de modification qui permet de modifier le titre des livres

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'sport' => 'pages#sport'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
+  get 'books/:id' => 'books#show'
+  patch 'books/:id' => 'books#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
