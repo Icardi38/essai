76e3f6eef48874377223e41d3140fbf29c795f72
Icardi38 <vm26190@hotmail.fr>
2018-02-01 09:11:37 +0100

    rien

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'sport' => 'pages#sport'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
